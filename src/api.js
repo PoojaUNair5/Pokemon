e9d0a9d8255c5cf2ac9c47958acd5580b1dd64fa
PoojaUNair5 <pooja.nair@robosoftin.com>
2020-11-19 21:44:21 +0530

    just after installing lodash

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ const BASE_URL = "https://pokeapi.co/api/v2";
 
 async function fetchpokemonURL (url) {
     let response = await fetch(url);
-
     if (response.ok) { // if HTTP-status is 200-299
       // get the response body (the method explained below)
     //   let json = await response.json();
--- src/api.test.js
+++ src/api.test.js
@@ -1,11 +1,12 @@
-import { fetchAPokemon, fetchpokemonURL } from "./api";
+import { fetchAPokemon, fetchpokemonURL } from './api';
+import lget from 'lodash/get';
 // if("fetchPokemon test", () => {
 //     return fetchPokemon().then((json) => {
 //         expect(json).not.toBeNull();
 //     )}
 // });
 
-it("fetchpokemon test", () => {
+it('fetchpokemon test', () => {
   jest.setTimeout(300000);
 
   //then- sucess function we expect json
@@ -16,24 +17,30 @@ it("fetchpokemon test", () => {
   });
 });
 
-it('fetchpokemon test', async () => {
+it('fetchpokemon test-', async () => {
   jest.setTimeout(300000);
 
-  const ditto = "ditto";
+  const ditto = 'ditto';
   //then- sucess function we expect json
   return fetchAPokemon(ditto).then((json) => {
     expect(json).not.toBeNull();
     // let results= json.results;
-      // let results = json.sprites.other.front_default;
-    // front artwork 
-    const sprites = json.sprites;
-    expect(sprites).not.toBeNull();
-    const other = sprites.other;
-    expect(other).not.toBeNull();
-    const official_artwork = other.official_artwork;
-    expect(official_artwork).not.toBeNull();
-    const front_default = other.front_default;
-    expect(front_default).not.toBeNull();
+    // let front_artwork = json?.sprites?.other?.official_artwork?.front_default;
+    //json.?sprites?.other?.{'official-artwork'}?.front_default
+    // front artwork
+    // const sprites = json.sprites;
+    // expect(sprites).not.toBeUndefined();
+    // const other = sprites.other;
+    // expect(other).not.toBeUndefined();
+    // const official_artwork = other['official_artwork'];
+    // expect(official_artwork).not.toBeUndefined();
+    // const front_default = official_artwork.front_default;
     // expect(results.length).toBe(100);
+
+    const front_default = lget(
+      json,
+      "sprites.other.official-artwork.front_default"
+    );
+    expect(front_default).not.toBeUndefined();
   }, 30000);
 });
